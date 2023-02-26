2993e09ea47d0eea442fcfba792411aea748228e
Augusto Seta <alseta97@gmail.com>
2023-02-25 22:40:39 -0300

    A Type component was created that will render the information that shows what type the Pokemon belongs to.

---
+++ src/components/PokemonCard/type/Type.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TypeStyled } from './TypeStyles'
+
+const Type = ({ typeName }) => {
+  return (
+    <TypeStyled type={ typeName } >{ typeName }</TypeStyled>
+  )
+}
+
+export default Type
