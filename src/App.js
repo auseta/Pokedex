3c0b177e9a0c11bc24d9377df71079b2dba06cac
Augusto Seta <alseta97@gmail.com>
2023-02-26 17:50:14 -0300

    Imported the useAxios custom hook and refactored the application code.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+import React from 'react'
 import Search from "./components/search/Search";
 import Pokemon from './components/PokemonCard/card/Card'
 import Loader from './components/loader/Loader'
 import { MyAppStyled } from './AppStyles';
 import GlobalStyles from './styles/GlobalStyles';
+import { useAxios } from './hooks/useAxios';
 
 function App() {
-  const [data, setData] = useState()
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(false)
 
- const handleSubmit = async (e, pokemon) => {
-    e.preventDefault()
-    setData(null);
-    setError(false);
-    setIsLoading(true)
-
-  try {
-    let selectedPokemon = pokemon.toLowerCase().trim()
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`)
-    setData(data)
-  } catch (error) {
-    setError("Pokemon no encontrado, trata de memorizarte mejor los names 🤡")
-  }
-  setIsLoading(false)
- }
+  const { data, isLoading, error, handleSubmit } = useAxios()
 
   return (
     <>
