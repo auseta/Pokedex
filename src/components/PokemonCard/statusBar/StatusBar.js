1424dd859e8a6c08319a119fa82dcec0e2ca8923
Augusto Seta <alseta97@gmail.com>
2023-02-25 22:33:48 -0300

    Cree un componente StatusBar que representará una barra de estado de una característica de Pokémon.

---
+++ src/components/PokemonCard/statusBar/StatusBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+  StatusBarContainerStyled,
+  StatusBarActiveStyled,
+  StatusBarLineStyled,
+  StatusBarTitleStyled
+} from './StatusBarStyles'
+
+const StatusBar = (props) => {
+
+  const {
+    base_stat,
+    stat: { name },
+    firstType,
+  } = props
+
+  return (
+    <StatusBarContainerStyled>
+      <StatusBarTitleStyled>{ name }</StatusBarTitleStyled>
+      <StatusBarLineStyled>
+        <StatusBarActiveStyled
+          power={ base_stat }
+          type={ firstType }
+        ></StatusBarActiveStyled>
+      </StatusBarLineStyled>
+    </StatusBarContainerStyled>
+  )
+}
+
+export default StatusBar
